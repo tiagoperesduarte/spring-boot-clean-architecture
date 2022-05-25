94becd3dd77079115b5ac29b1bae8f0d14be681c
Tiago Duarte <tiago.duarte@ifood.com.br>
2022-05-25 10:47:46 -0300

    Adicionado microservice que ira intermediar o controle de meios de pagamento entre áreas e restaurantes

---
+++ rpmc/src/test/java/com/delivery/rpmc/RestaurantPaymentMethodControlApplicationTests.java
@@ -0,0 +1,11 @@
+package com.delivery.rpmc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantPaymentMethodControlApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
